6e823c289e9d92d16ae2617ff9f55595486a1769
CatiaAzevedo <catiaazevedo52@gmail.com>
2025-05-08 22:11:27 +0100

    update

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,8 +2,8 @@ window.addEventListener('scroll', () => {
   const logo = document.getElementById('logo');
   const nav = document.querySelector('.nav1');
 
-  const threshold = 55;
-  const margin = 54;
+  const threshold = 50;
+  const margin = 49;
 
   if (window.scrollY > threshold + margin) {
     logo.classList.add('hidden');
